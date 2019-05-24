f09419af95d9aa806353027f926c0cc49029b1d4
dejan <dejan.ercegan@gmail.com>
2019-05-24 19:28:23 +0200

    works

--- file_helper.py
+++ file_helper.py
@@ -3,24 +3,28 @@ import argparse
 
 
 def listDir(dir):
+    dir = os.path.join(dir)
+    print(dir)
     fileNames = os.listdir(dir)
     for fileName in fileNames:
         print('File Name: ' + fileName)
         print('Folder Path: ' + os.path.abspath(os.path.join(dir, fileName)), sep='\n')
+    return
 
 def arguments_parsing():
 
     parser = argparse.ArgumentParser(description='files path')
-    parser.add_argument("--path", type="dir_path",
+    parser.add_argument("--path", required=True,
                         help="display a path to the directory")
 
     args = parser.parse_args()
+    print(args)
     return args
 
 def file_helper():
     args = arguments_parsing()
     print(args.path)
-    print(listDir)
+    listDir(args.path)
 
 if __name__ == '__main__':
     file_helper()
